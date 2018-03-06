37d5cb78100f4c54b0798fa33fba14c30a02ac0b
roman <berezdecky98@gmail.com>
2018-03-06 17:33:55 +0200

    save and get templates

--- app.js
+++ app.js
@@ -1,23 +1,46 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const updater = require('jsonfile-updater');
+const jsonfile = require('jsonfile');
+const base64Img = require('base64-img');
+const util = require('util');
 
 const { getFonts } = require('./get_data');
+const file = './templates.json';
 
 const app = express();
 app.use(cors());
+app.use(express.static('public'));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.json());
 
 app.get('/getFonts', (req, res) => {
-    // let fonts = await getFonts();
     setTimeout(() => {
         res.send(JSON.stringify(['Arial', 'Gadget', 'Charcoal', 'monospace', 'Georgia', 'Palatino', 'cursive', 'Verdana']));
     }, 500);
     
 });
 
-app.get('/getImages', (req, res) => {
+app.get('/getImages', async (req, res) => {
+    const toBase64 = util.promisify(base64Img.base64);
+    let img1 = await toBase64(__dirname + '/public/images/1.png');
+    let img2 = await toBase64(__dirname + '/public/images/2.jpg');
     setTimeout(() => {
-        res.send(JSON.stringify(["https://i.ytimg.com/vi/yA30K3z5PSw/hqdefault_live.jpg","https://i.ytimg.com/vi/yA30K3z5PSw/hqdefault_live.jpg", "https://i.ytimg.com/vi/yA30K3z5PSw/hqdefault_live.jpg", "https://i.ytimg.com/vi/yA30K3z5PSw/hqdefault_live.jpg", "https://i.ytimg.com/vi/yA30K3z5PSw/hqdefault_live.jpg","https://i.ytimg.com/vi/yA30K3z5PSw/hqdefault_live.jpg","https://i.ytimg.com/vi/yA30K3z5PSw/hqdefault_live.jpg","https://i.ytimg.com/vi/yA30K3z5PSw/hqdefault_live.jpg", "https://i.ytimg.com/vi/yA30K3z5PSw/hqdefault_live.jpg"]));        
+        res.send(JSON.stringify([img2, img1, img2, img1, img2]));        
     }, 1000);
+});
+
+app.post('/setTemplate', async (req, res) => {
+    await updater(file).append('templates', JSON.stringify(req.body));
+    res.send('OK');
+});
+
+app.get('/getTemplates', async (req, res) => {
+    let readFile = util.promisify(jsonfile.readFile);
+    await readFile(file).then(data => {
+        res.send(data);
+    });
 });
 
 app.listen(3300, () => {
