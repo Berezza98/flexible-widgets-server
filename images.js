2e3cf63baa61ccadaeaca79234b991ff4d50e0b9
roman <berezdecky98@gmail.com>
2018-06-07 17:43:31 +0300

    16 images per page

--- images.js
+++ images.js
@@ -97,6 +97,102 @@ let images = [
     {
         name: "Image 12 NEW",
         src: img2
+    },
+    {
+        name: "Image 1**",
+        src: img1
+    },
+    {
+        name: "Image 2**",
+        src: img2
+    },
+    {
+        name: "Image 3**",
+        src: img1
+    },
+    {
+        name: "Image 4**",
+        src: img2
+    },
+    {
+        name: "Image 5**",
+        src: img1
+    },
+    {
+        name: "Image 6**",
+        src: img2
+    },
+    {
+        name: "Image 7**",
+        src: img1
+    },
+    {
+        name: "Image 8**",
+        src: img2
+    },
+    {
+        name: "Image 9**",
+        src: img1
+    },
+    {
+        name: "Image 10**",
+        src: img2
+    },
+    {
+        name: "Image 11**",
+        src: img1
+    },
+    {
+        name: "Image 12**",
+        src: img2
+    },
+    {
+        name: "Image 1 NEW**",
+        src: img1
+    },
+    {
+        name: "Image 2 NEW**",
+        src: img2
+    },
+    {
+        name: "Image 3 NEW**",
+        src: img1
+    },
+    {
+        name: "Image 4 NEW**",
+        src: img2
+    },
+    {
+        name: "Image 5 NEW**",
+        src: img1
+    },
+    {
+        name: "Image 6 NEW**",
+        src: img2
+    },
+    {
+        name: "Image 7 NEW**",
+        src: img1
+    },
+    {
+        name: "Image 8 NEW**",
+        src: img2
+    },
+    {
+        name: "Image 9 NEW**",
+        src: img1
+    },
+    {
+        name: "Image 10 NEW**",
+        src: img2
+    },
+    {
+        name: "Image 11 NEW**",
+        src: img1
+    },
+    {
+        name: "Image 12 NEW**",
+        src: img2
     }
 ];
 
