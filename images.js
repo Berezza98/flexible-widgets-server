409d789d9c5c4c5aef6f342fae29e42bdf69c6a5
Roman Berezdecky <berezdecky98@gmail.com>
2018-07-18 12:56:53 +0200

    image categories

--- images.js
+++ images.js
@@ -4,195 +4,243 @@ let img2 = 'https://flexible-app.herokuapp.com/images/2.jpg';
 let images = [
     {
         name: "Image 1",
-        src: img1
+        src: img1,
+        id: 0
     },
     {
         name: "Image 2",
-        src: img2
+        src: img2,
+        id: 1
     },
     {
         name: "Image 3",
-        src: img1
+        src: img1,
+        id: 2
     },
     {
         name: "Image 4",
-        src: img2
+        src: img2,
+        id: 3
     },
     {
         name: "Image 5",
-        src: img1
+        src: img1,
+        id: 4
     },
     {
         name: "Image 6",
-        src: img2
+        src: img2,
+        id: 5
     },
     {
         name: "Image 7",
-        src: img1
+        src: img1,
+        id: 6
     },
     {
         name: "Image 8",
-        src: img2
+        src: img2,
+        id: 7
     },
     {
         name: "Image 9",
-        src: img1
+        src: img1,
+        id: 8
     },
     {
         name: "Image 10",
-        src: img2
+        src: img2,
+        id: 9
     },
     {
         name: "Image 11",
-        src: img1
+        src: img1,
+        id: 10
     },
     {
         name: "Image 12",
-        src: img2
+        src: img2,
+        id: 11
     },
     {
         name: "Image 1 NEW",
-        src: img1
+        src: img1,
+        id: 12
     },
     {
         name: "Image 2 NEW",
-        src: img2
+        src: img2,
+        id: 13
     },
     {
         name: "Image 3 NEW",
-        src: img1
+        src: img1,
+        id: 14
     },
     {
         name: "Image 4 NEW",
-        src: img2
+        src: img2,
+        id: 15
     },
     {
         name: "Image 5 NEW",
-        src: img1
+        src: img1,
+        id: 16
     },
     {
         name: "Image 6 NEW",
-        src: img2
+        src: img2,
+        id: 17
     },
     {
         name: "Image 7 NEW",
-        src: img1
+        src: img1,
+        id: 18
     },
     {
         name: "Image 8 NEW",
-        src: img2
+        src: img2,
+        id: 19
     },
     {
         name: "Image 9 NEW",
-        src: img1
+        src: img1,
+        id: 20
     },
     {
         name: "Image 10 NEW",
-        src: img2
+        src: img2,
+        id: 21
     },
     {
         name: "Image 11 NEW",
-        src: img1
+        src: img1,
+        id: 22
     },
     {
         name: "Image 12 NEW",
-        src: img2
+        src: img2,
+        id: 
     },
     {
         name: "Image 1**",
-        src: img1
+        src: img1,
+        id: 23
     },
     {
         name: "Image 2**",
-        src: img2
+        src: img2,
+        id: 24
     },
     {
         name: "Image 3**",
-        src: img1
+        src: img1,
+        id: 25
     },
     {
         name: "Image 4**",
-        src: img2
+        src: img2,
+        id: 26
     },
     {
         name: "Image 5**",
-        src: img1
+        src: img1,
+        id: 27
     },
     {
         name: "Image 6**",
-        src: img2
+        src: img2,
+        id: 28
     },
     {
         name: "Image 7**",
-        src: img1
+        src: img1,
+        id: 29
     },
     {
         name: "Image 8**",
-        src: img2
+        src: img2,
+        id: 30
     },
     {
         name: "Image 9**",
-        src: img1
+        src: img1,
+        id: 31
     },
     {
         name: "Image 10**",
-        src: img2
+        src: img2,
+        id: 32
     },
     {
         name: "Image 11**",
-        src: img1
+        src: img1,
+        id: 33
     },
     {
         name: "Image 12**",
-        src: img2
+        src: img2,
+        id: 34
     },
     {
         name: "Image 1 NEW**",
-        src: img1
+        src: img1,
+        id: 35
     },
     {
         name: "Image 2 NEW**",
-        src: img2
+        src: img2,
+        id: 36
     },
     {
         name: "Image 3 NEW**",
-        src: img1
+        src: img1,
+        id: 37
     },
     {
         name: "Image 4 NEW**",
-        src: img2
+        src: img2,
+        id: 38
     },
     {
         name: "Image 5 NEW**",
-        src: img1
+        src: img1,
+        id: 
     },
     {
         name: "Image 6 NEW**",
-        src: img2
+        src: img2,
+        id: 
     },
     {
         name: "Image 7 NEW**",
-        src: img1
+        src: img1,
+        id: 39
     },
     {
         name: "Image 8 NEW**",
-        src: img2
+        src: img2,
+        id: 40
     },
     {
         name: "Image 9 NEW**",
-        src: img1
+        src: img1,
+        id: 41
     },
     {
         name: "Image 10 NEW**",
-        src: img2
+        src: img2,
+        id: 42
     },
     {
         name: "Image 11 NEW**",
-        src: img1
+        src: img1,
+        id: 43
     },
     {
         name: "Image 12 NEW**",
-        src: img2
+        src: img2,
+        id: 
     }
 ];
 
